72447b582129a0196d5bfd6bbafb4e2754b2e293
nyirurugo omar <nyirurugoomar@gmail.com>
2025-12-16 18:32:17 +0200

    Initial commit

---
+++ src/test/java/com/example/rdm_service_platform/RdmServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rdm_service_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdmServicePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
