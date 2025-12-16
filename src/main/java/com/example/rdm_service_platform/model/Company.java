345ad05c2af242d8fbc009795d16640a40b4e88c
nyirurugo omar <nyirurugoomar@gmail.com>
2025-12-16 19:23:02 +0200

    Create company model

---
+++ src/main/java/com/example/rdm_service_platform/model/Company.java
@@ -0,0 +1,43 @@
+
+package com.example.rdm_service_platform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+
+@Entity
+@Table(name = "companies")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    
+
+}
